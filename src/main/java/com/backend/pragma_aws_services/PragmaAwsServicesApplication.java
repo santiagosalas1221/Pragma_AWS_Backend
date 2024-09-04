c94683ff364ac3d9be2b4699fffa4647438a60e2
Santiago Salas <santiago.salas.contractor@bbva.com>
2024-09-04 11:19:15 -0500

    Start project

---
+++ src/main/java/com/backend/pragma_aws_services/PragmaAwsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.backend.pragma_aws_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PragmaAwsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PragmaAwsServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/pragma_aws_services/PragmaAwsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.pragma_aws_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PragmaAwsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
